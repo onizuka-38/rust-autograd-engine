f31656fa080dd81ec79d2458797dc2f4f5d7b7c7
onizuka-38 <bdh9938@gmail.com>
2026-03-31 10:55:44 +0900

    sequential update

---
+++ src/nn/sequential.rs
@@ -0,0 +1,34 @@
+﻿use crate::nn::Module;
+use crate::Tensor;
+
+pub struct Sequential {
+    layers: Vec<Box<dyn Module>>,
+}
+
+impl Sequential {
+    pub fn new() -> Self {
+        Self { layers: Vec::new() }
+    }
+
+    pub fn add<M: Module + 'static>(&mut self, layer: M) {
+        self.layers.push(Box::new(layer));
+    }
+}
+
+impl Module for Sequential {
+    fn forward(&self, input: &Tensor) -> Tensor {
+        let mut out = input.clone();
+        for layer in &self.layers {
+            out = layer.forward(&out);
+        }
+        out
+    }
+
+    fn parameters(&self) -> Vec<Tensor> {
+        let mut params = Vec::new();
+        for layer in &self.layers {
+            params.extend(layer.parameters());
+        }
+        params
+    }
+}
